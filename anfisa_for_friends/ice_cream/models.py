d9d58a59877660859d41dc95d086940e60322bf7
Sergey Sednev <SednevSergey43@gmail.com>
2025-07-25 12:40:24 +0300

    Спринт

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -6,7 +6,8 @@ from core.models import PublishedModel
 class Category(PublishedModel):
     title = models.CharField(max_length=256, verbose_name='Название')
     slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
-    output_order = models.PositiveSmallIntegerField(default=100, verbose_name='Порядок отображения')
+    output_order = models.PositiveSmallIntegerField(
+        default=100, verbose_name='Порядок отображения')
 
     class Meta:
         verbose_name = 'категория'
@@ -29,7 +30,8 @@ class Topping(PublishedModel):
 
 
 class Wrapper(PublishedModel):
-    title = models.CharField('Название', max_length=256,help_text='Уникальное название обёртки, не более 256 символов')
+    title = models.CharField('Название', max_length=256,
+                             help_text='Уникальное название обёртки, не более 256 символов')
 
     class Meta:
         verbose_name = 'объект «Обёртка»'
@@ -58,10 +60,16 @@ class IceCream(PublishedModel):
     )
     toppings = models.ManyToManyField(Topping, verbose_name='Топинг')
     is_on_main = models.BooleanField('На главную', default=False)
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     class Meta:
         verbose_name = 'Мороженое'
         verbose_name_plural = 'Мороженое'
+        ordering = ('output_order', 'title')
 
     def __str__(self):
         return self.title
